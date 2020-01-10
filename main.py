71f2fc948c57e074660b7c802aca82b9b7dc64de
Svilen Stavrev <svilen@etherino.net>
2020-01-10 18:49:09 +0200

    Working on periodic transmittions over Internet and APRS. APRS part is not finished. COde is not tested

--- main.py
+++ main.py
@@ -8,8 +8,35 @@ from socket import error as socket_error
 
 ptt = LED(4)
 ptt.off()
-aprs_counter = 1
 
+# Every time when broadcast message is received from a sensor the data from it is parsed and stored to aprs_data and
+# mqtt_data dictionaries. Tke keys are var names in format <mac address>_<data type>. Values are the value measured by
+# the sensor. For example 1d:aa:44_temp=23. If data for var with name already in dict is received the value is updated.
+# Data from dicts is sent periodically and after this dictionaries are cleared.
+aprs_data = {}
+mqtt_data = {}
+aprs_tx_period = datetime.timedelta(minutes=3)
+mqtt_tx_period = datetime.timedelta(minutes=3)
+aprs_last_tx_timestamp = datetime.datetime.now()
+mqtt_last_tx_timestamp = datetime.datetime.now()
+
+aprs_var_name_translation = {
+    '1d:aa:44_temp': 'T1',  # Garage
+    '1d:ab:43_temp': 'T2',  # Fl.2
+    '1d:a7:d1_temp': 'T3',  # Fl.1
+    '77:b7:bf_P' : 'P',  # Atm. pressure
+    '77:b7:bf_temp': 'Tout',  # Temperature outside
+    '77:b7:bf_hum' : 'Rh',  # Humidity outside
+    '77:b7:bf_tempBMP': 'Tin',  # Temperature inside, measured on sensor board
+    '77:b7:bf_wd': 'WD',  # Wind direction
+    '77:b7:bf_ws': 'WS',  # Wind speed
+    '77:b7:bf_wg': 'WG',  # Wind gusts
+    '77:b7:bf_rain1h': 'R1',  # Rain for the last 1 hour
+    '77:b7:bf_rain24h': 'R24',  # Rain for the last 24 hours
+    '77:b7:bf_Ubat': 'U'    # Battery voltage
+}
+
+# TX20 wind sensors sends a number from 0 to 15 for wind direction. Here is conversion to string name
 wind_dir_dictionary = {
     0:  "N",
     1:  "NNE",
@@ -40,29 +67,36 @@ rain_data_24h = {}
 # prepare for multicast receive
 mcast_port = 8888
 mcast_grp = "224.0.0.120"
-#interface_ip = str(INADDR_ANY)
-interface_ip = str("192.168.152.90")
-
+# interface_ip = str(INADDR_ANY)
+interface_ip = str("192.168.152.90")    #  Sensors obtain their IP by DHCP starting from 192.168.152.100
+                                        #  RaspberryPi board has fixed IP address in LAN
 s = socket(AF_INET, SOCK_DGRAM)
 s.bind(("", mcast_port))
 mreq = inet_aton(mcast_grp) + inet_aton(interface_ip)
-
 s.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, str(mreq))
 
 # prepare MQTT broker connection
 # https://customer.cloudmqtt.com/login
 # https://1sheeld.com/mqtt-protocol/
-
+client = mqtt.Client(client_id='*******')  # TODO: Да проверя в RaspPi дали е същото!!
+client.username_pw_set('**********', '********')
+client.on_connect = on_connect
 
 
 def on_connect(client, userdata, flags, rc):
+    # This function does nothing. Just for information
     if rc == 0:
         print("Connected to broker OK!")
     else:
         print("Failed connecting to broker. Returned code:", rc)
     return 0
 
-def aprs(data):
+
+def aprs(aprs_data_string):
+    # This function takes a string which should contain sensors data and transmit, adds APRS header including
+    # APRS symbol, GPS coordinates and transmits it over radio. PTT is triggered by GPIO  .
+    # Direwolf shall be started on on power on. This function connects to Direwolf on port 8001.
+
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.connect(('127.0.0.1', 8001))
 
@@ -78,7 +112,7 @@ def aprs(data):
     msgHeader = '!4307.46N/02744.14Ey '  # Zdravets, house with Yagi
 
     # msgBody = 'T_garage=15 T_fl.2=23 Tfl.1=18 P=1008hPa   TEST 11'
-    msgBody = data
+    msgBody = aprs_data_string
 
     msg = chr(0xC0)
     msg += chr(0x00)
@@ -101,18 +135,13 @@ def aprs(data):
     msg += msgBody
     msg += chr(0xC0)
 
-    prit('Sending APRS data...')
+    prit('Sending APRS data:', msgBody)
     ptt.on()
     time.sleep(0.1)
     soc.send(msg)
     time.sleep(2)
     ptt.off()
     return 0
-
-
-client = mqtt.Client(client_id='******')
-client.username_pw_set('******', '******')
-client.on_connect = on_connect
 
 
 # Receive multicast data and send it to MQTT broker  loop
@@ -124,7 +153,7 @@ while 1:
     print(data)
     print
 
-    # parse data
+    # parse data inside multicast packet
     tempAM = -100
     hum = -100
     wind_dir = -100
@@ -138,7 +167,7 @@ while 1:
     mac = '00:00:00'  # last 3 bytes only
     hour = datetime.timedelta(minutes=60)
     day = datetime.timedelta(hours=24)
-    publish_rain = False
+    update_rain = False  # this flag is turned to True. This is to update rain values only when new data is available
 
     lines = data.splitlines()
     if lines[0].startswith('MAC='):
@@ -180,9 +209,8 @@ while 1:
                 nums = re.findall(r'-?\d+', ln)
                 tempBMP = nums[1]
                 pressure = nums[2]
-
             if ln.startswith("RAIN:"):
-                publish_rain = True
+                update_rain = True
                 nums = re.findall(r'-?\d+', ln)
                 rain = nums[0]
                 timestamp = datetime.datetime.now()
@@ -206,45 +234,64 @@ while 1:
             if ln.startswith("Ubat:"):
                 nums = re.findall(r'[-+]?\d*\.*\d+', ln)
                 ubat = nums[0]
+    else:
+        continue    # Maybe malformed data
 
-        # send to MQTT broker
+    # store parsed data aprs_data and mqtt_data dictionaries
+    if tempAM != -100:
+        aprs_data[mac + '_' + 'temp'] = tempAM
+        mqtt_data[mac + '_' + 'temp'] = tempAM
+    if hum != -100:
+        aprs_data[mac + '_' + 'hum'] = hum
+        mqtt_data[mac + '_' + 'hum'] = hum
+    if pressure != -100:
+        aprs_data[mac + '_' + 'P'] = pressure
+        mqtt_data[mac + '_' + 'P'] = pressure
+    if tempBMP != -100:
+        aprs_data[mac + '_' + 'tempBMP'] = tempBMP
+        mqtt_data[mac + '_' + 'tempBMP'] = tempBMP
+    if ubat != -100:
+        aprs_data[mac + '_' + 'Ubat'] = ubat
+        mqtt_data[mac + '_' + 'Ubat'] = ubat
+    #  process new rain data
+    if update_rain:
+        rain_1h = 0
+        if len(rain_data_1h) != 0:
+            for t in rain_data_1h:
+                rain_1h += int(rain_data_1h[t])
+        aprs_data[mac + '_' + 'rain1h'] = rain_1h
+        mqtt_data[mac + '_' + 'rain1h'] = rain_1h
+        rain_24h = 0
+        if len(rain_data_24h) != 0:
+            for t in rain_data_24h:
+                rain_24h += int(rain_data_24h[t])
+        aprs_data[mac + '_' + 'rain24h'] = rain_24h
+        mqtt_data[mac + '_' + 'rain24h'] = rain_24h
+    # process wind data
+    if wind_speed != -100:
+        aprs_data[mac + '_' + 'ws'] = average_speed
+        aprs_data[mac + '_' + 'wg'] = gusts
+        aprs_data[mac + '_' + 'wd'] = wind_dir_dictionary[average_dir]
+        mqtt_data[mac + '_' + 'ws'] = average_speed
+        mqtt_data[mac + '_' + 'wg'] = gusts
+        mqtt_data[mac + '_' + 'wd'] = wind_dir_dictionary[average_dir]
+
+    # check if its time to send to MQTT broker.
+    timestamp = datetime.datetime.now()
+    if (timestamp - mqtt_last_tx_timestamp) > mqtt_tx_period:
         try:
             client.connect('farmer.cloudmqtt.com', 10791)
-
-            if tempAM != -100:
-                client.publish(mac + '_' + 'temp', tempAM)
-            if hum != -100:
-                client.publish(mac + '_' + 'hum', hum)
-            if pressure != -100:
-                client.publish(mac + '_' + 'P', pressure)
-            if tempBMP != -100:
-                client.publish(mac + '_' + 'tempBMP', tempBMP)
-            if ubat != -100:
-                client.publish(mac + '_' + 'Ubat', ubat)
-            #  publish rain
-            if publish_rain:
-                rain_1h = 0
-                if len(rain_data_1h) != 0:
-                    for t in rain_data_1h:
-                        rain_1h += int(rain_data_1h[t])
-                client.publish(mac + '_' + 'rain1h', rain_1h)
-                rain_24h = 0
-                if len(rain_data_24h) != 0:
-                    for t in rain_data_24h:
-                        rain_24h += int(rain_data_24h[t])
-                client.publish(mac + '_' + 'rain24h', rain_24h)
-            # publish wind
-            if wind_speed != -100:
-                client.publish(mac + '_' + 'ws', average_speed)
-                client.publish(mac + '_' + 'wg', gusts)
-                client.publish(mac + '_' + 'wd', wind_dir_dictionary[average_dir])
+            for aprs_data_key in aprs_data:
+                client.publish(aprs_data_key, aprs_data[aprs_data_key])
             client.disconnect()
+            mqtt_last_tx_timestamp = timestamp
         except socket_error as err:
             print("ERR: Failed publish data to broker")
             print err
             print
 
-        # send APRS
-        aprs_counter += 1
-        if (aprs_counter%5)==0:
-            aprs('test data msg ', aprs_counter)
+    # check if its time to transmit APRS over radio.
+    timestamp = datetime.datetime.now()
+    if (timestamp - aprs_last_tx_timestamp) > aprs_tx_period:
+        # TODO: prepare aprs message
+        aprs('test data msg ', aprs_counter)
