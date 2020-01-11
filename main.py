d645255bee84f441feee5130b1e221b641642a17
Svilen Stavrev <svilen@etherino.net>
2020-01-11 11:50:20 +0200

    Tested on PC. MQTT publish is working. APRS is building the message. TODO: Test on RaspberryPi with APRS radio TX

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 from socket import *
-from gpiozero import LED
+# from gpiozero import LED  TODO
 import datetime
+import time
 import re
 import paho.mqtt.client as mqtt
 from socket import error as socket_error
 
-ptt = LED(4)
-ptt.off()
+# ptt = LED(4)
+# ptt.off()   TODO
 
 # Every time when broadcast message is received from a sensor the data from it is parsed and stored to aprs_data and
 # mqtt_data dictionaries. Tke keys are var names in format <mac address>_<data type>. Values are the value measured by
@@ -15,25 +16,25 @@ ptt.off()
 # Data from dicts is sent periodically and after this dictionaries are cleared.
 aprs_data = {}
 mqtt_data = {}
-aprs_tx_period = datetime.timedelta(minutes=3)
-mqtt_tx_period = datetime.timedelta(minutes=3)
+aprs_tx_period = datetime.timedelta(minutes=2)
+mqtt_tx_period = datetime.timedelta(minutes=2)
 aprs_last_tx_timestamp = datetime.datetime.now()
 mqtt_last_tx_timestamp = datetime.datetime.now()
 
 aprs_var_name_translation = {
-    '1d:aa:44_temp': 'T1',  # Garage
-    '1d:ab:43_temp': 'T2',  # Fl.2
-    '1d:a7:d1_temp': 'T3',  # Fl.1
-    '77:b7:bf_P' : 'P',  # Atm. pressure
-    '77:b7:bf_temp': 'Tout',  # Temperature outside
-    '77:b7:bf_hum' : 'Rh',  # Humidity outside
-    '77:b7:bf_tempBMP': 'Tin',  # Temperature inside, measured on sensor board
-    '77:b7:bf_wd': 'WD',  # Wind direction
-    '77:b7:bf_ws': 'WS',  # Wind speed
-    '77:b7:bf_wg': 'WG',  # Wind gusts
-    '77:b7:bf_rain1h': 'R1',  # Rain for the last 1 hour
-    '77:b7:bf_rain24h': 'R24',  # Rain for the last 24 hours
-    '77:b7:bf_Ubat': 'U'    # Battery voltage
+    '1d:aa:44_temp': ' T1=',  # Garage
+    '1d:ab:43_temp': ' T2=',  # Fl.2
+    '1d:a7:d1_temp': ' T3=',  # Fl.1
+    '77:b7:bf_P': ' P=',  # Atm. pressure
+    '77:b7:bf_temp': ' Tout=',  # Temperature outside
+    '77:b7:bf_hum': ' Rh=',  # Humidity outside
+    '77:b7:bf_tempBMP': ' Tin',  # Temperature inside, measured on sensor board
+    '77:b7:bf_wd': ' WD=',  # Wind direction
+    '77:b7:bf_ws': ' WS=',  # Wind speed
+    '77:b7:bf_wg': ' WG=',  # Wind gusts
+    '77:b7:bf_rain1h': ' R1=',  # Rain for the last 1 hour
+    '77:b7:bf_rain24h': ' R24=',  # Rain for the last 24 hours
+    '77:b7:bf_Ubat': ' U='    # Battery voltage
 }
 
 # TX20 wind sensors sends a number from 0 to 15 for wind direction. Here is conversion to string name
@@ -67,9 +68,9 @@ rain_data_24h = {}
 # prepare for multicast receive
 mcast_port = 8888
 mcast_grp = "224.0.0.120"
-# interface_ip = str(INADDR_ANY)
-interface_ip = str("192.168.152.90")    #  Sensors obtain their IP by DHCP starting from 192.168.152.100
-                                        #  RaspberryPi board has fixed IP address in LAN
+# interface_ip = str(INADDR_ANY)         # Sensors obtain their IP by DHCP starting from 192.168.152.100
+interface_ip = str("192.168.152.91")    # RaspberryPi board has fixed IP address in LAN
+
 s = socket(AF_INET, SOCK_DGRAM)
 s.bind(("", mcast_port))
 mreq = inet_aton(mcast_grp) + inet_aton(interface_ip)
@@ -78,9 +79,8 @@ s.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, str(mreq))
 # prepare MQTT broker connection
 # https://customer.cloudmqtt.com/login
 # https://1sheeld.com/mqtt-protocol/
-client = mqtt.Client(client_id='*******')  # TODO: Да проверя в RaspPi дали е същото!!
-client.username_pw_set('**********', '********')
-client.on_connect = on_connect
+client = mqtt.Client(client_id='*************') 
+client.username_pw_set('******', '*********')
 
 
 def on_connect(client, userdata, flags, rc):
@@ -92,34 +92,39 @@ def on_connect(client, userdata, flags, rc):
     return 0
 
 
+client.on_connect = on_connect
+
+
 def aprs(aprs_data_string):
     # This function takes a string which should contain sensors data and transmit, adds APRS header including
     # APRS symbol, GPS coordinates and transmits it over radio. PTT is triggered by GPIO  .
     # Direwolf shall be started on on power on. This function connects to Direwolf on port 8001.
 
+    print("Sending APRS:", aprs_data_string)
+    return 0
+
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.connect(('127.0.0.1', 8001))
 
-    # msgBody = '!4323.28H/02789.64E>TEST'
-    # msgBody = '!4313.98NW02753.78E# TEST 6'
-    # msgBody = '!4313.98N/02753.78Ey c999s999g008t054r001 TEST 20'
+    # msg_body = '!4323.28H/02789.64E>TEST'
+    # msg_body = '!4313.98NW02753.78E# TEST 6'
+    # msg_body = '!4313.98N/02753.78Ey c999s999g008t054r001 TEST 20'
     # the leter after N  between long and alt is the overlay char. / for none
     # the leter after E after long and alt is the symbol. _ weather station, y house with yagi, > car
 
     #               DST       SRC      DIGI-->
     callsigns = ['LZ2SMX9 ', 'LZ2SMX3', 'WIDE2 1']
-    # msgHeader = '!4313.98N/02753.78Ey '  # vazrajdane 66, house with Yagi
-    msgHeader = '!4307.46N/02744.14Ey '  # Zdravets, house with Yagi
+    # msg_header = '!4313.98N/02753.78Ey '  # vazrajdane 66, house with Yagi
+    msg_header = '!4307.46N/02744.14Ey '  # Zdravets, house with Yagi
 
-    # msgBody = 'T_garage=15 T_fl.2=23 Tfl.1=18 P=1008hPa   TEST 11'
-    msgBody = aprs_data_string
+    # msg_body = 'T_garage=15 T_fl.2=23 Tfl.1=18 P=1008hPa   TEST 11'
+    msg_body = aprs_data_string
 
     msg = chr(0xC0)
     msg += chr(0x00)
     for i in range(0, len(callsigns)):
         cs = callsigns[i]
         for j in range(0, 6):
-            t = cs[j]
             msg += chr(ord(cs[j]) << 1)
         ssid = ord(cs[6])
         ssid <<= 1
@@ -131,16 +136,16 @@ def aprs(aprs_data_string):
 
     msg += chr(0x03)
     msg += chr(0xF0)
-    msg += msgHeader
-    msg += msgBody
+    msg += msg_header
+    msg += msg_body
     msg += chr(0xC0)
 
-    prit('Sending APRS data:', msgBody)
-    ptt.on()
+    print('Sending APRS data:', msg_body)
+#    ptt.on()
     time.sleep(0.1)
     soc.send(msg)
     time.sleep(2)
-    ptt.off()
+#    ptt.off() TODO
     return 0
 
 
@@ -281,10 +286,14 @@ while 1:
     if (timestamp - mqtt_last_tx_timestamp) > mqtt_tx_period:
         try:
             client.connect('farmer.cloudmqtt.com', 10791)
-            for aprs_data_key in aprs_data:
-                client.publish(aprs_data_key, aprs_data[aprs_data_key])
+            print('Publish MQTT :')
+            for mqtt_data_key in mqtt_data:
+                client.publish(mqtt_data_key, mqtt_data[mqtt_data_key])
+                print(mqtt_data_key, '=', mqtt_data[mqtt_data_key])
             client.disconnect()
+            mqtt_data.clear()
             mqtt_last_tx_timestamp = timestamp
+
         except socket_error as err:
             print("ERR: Failed publish data to broker")
             print err
@@ -292,6 +301,12 @@ while 1:
 
     # check if its time to transmit APRS over radio.
     timestamp = datetime.datetime.now()
+    aprs_message = ''
     if (timestamp - aprs_last_tx_timestamp) > aprs_tx_period:
-        # TODO: prepare aprs message
-        aprs('test data msg ', aprs_counter)
+        aprs_last_tx_timestamp = timestamp
+        for aprs_data_key in aprs_data:
+            if aprs_data_key in aprs_var_name_translation.keys():
+                aprs_message += aprs_var_name_translation[aprs_data_key]
+                aprs_message += aprs_data[aprs_data_key]
+        aprs_data.clear()
+        aprs(aprs_message)
